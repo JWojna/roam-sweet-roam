6f21b8659e93c46024ba68593f0f7902a2b4fba9
Jacob <EarthTek19@gmail.com>
2024-12-04 23:57:20 -0600

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,19 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 //* test route
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
 
 //* Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
@@ -19,3 +27,4 @@ router.get("/api/csrf/restore", (req, res) => {
 
 //------- EXPORTS --------//
 module.exports = router;
+
