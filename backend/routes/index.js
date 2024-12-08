a49194568323c6410c8794c05a3f05a6d831e00f
Jacob <EarthTek19@gmail.com>
2024-12-08 16:49:50 -0600

    testing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,10 +7,10 @@ const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
 //* test route
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
 
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
